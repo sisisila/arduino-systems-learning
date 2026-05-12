c0e4abfbf4ae34fcd575245623d7a07f6946dce3
Sila <silabk13@gmail.com>
2026-05-11 22:16:14 -0400

    Add sketch 6.1.1

---
+++ Sketch-6.1.1-SendDataThruSerialPort/Sketch-6.1.1-SendDataThruSerialPort.ino
@@ -0,0 +1,13 @@
+int counter = 0; // define a variable as a data sending to serial port
+
+void setup() {
+  Serial.begin(9600); // initialize the serial port, set the baud rate to 9600
+}
+
+void loop() {
+  // print variable counter value to serial
+  Serial.print("counter:"); // print the string "counter:"
+  Serial.println(counter); // print the variable counter value
+  delay (500); // wait 500ms to avoid cycling too fast
+  counter++; // variable counter increases 1
+}
