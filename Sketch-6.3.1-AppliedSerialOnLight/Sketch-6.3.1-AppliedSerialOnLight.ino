85c26106d0639cc1a8226155e3da2f7c16ee4c63
Sila <silabk13@gmail.com>
2026-05-12 22:27:37 -0400

    Add sketch 6.3.1

---
+++ Sketch-6.3.1-AppliedSerialOnLight/Sketch-6.3.1-AppliedSerialOnLight.ino
@@ -0,0 +1,17 @@
+int inInt; // define a variable to store the data received from serial
+int ledPin = 11; // the number of the LED pin
+
+void setup() {
+  pinMode(ledPin, OUTPUT); // initialize the LED pin as an output
+  Serial.begin(9600); // initialize serial port, set baud rate to 9600
+}
+
+void loop() {
+  if (Serial.available()) { // judge whether the data has been received
+    inInt = Serial.parseInt(); // read an integer
+    Serial.print("received:"); // print the string " received:"
+    Serial.println(inInt); // print the received character
+    // convert the received integer into PWM duty cycle of ledPin port
+    analogWrite(ledPin, constrain(inInt, 0, 255));
+  }
+}
